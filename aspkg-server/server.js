613abef0706d36608cfcd91860f523180ef31e02
JairusSW <jairus.v.tanaka@outlook.com>
2021-07-12 11:02:30 -0700

    Initial server

---
+++ aspkg-server/server.js
@@ -0,0 +1,26 @@
+const { fastify } = require('fastify')
+
+const fs = require('fs')
+
+const path = require('path')
+
+const kati = require('kati')
+
+const app = fastify()
+
+app.listen(3000)
+
+// Static file serve
+app.register(require('fastify-static'), {
+    root: path.join(__dirname, '../aspkg-bss'),
+})
+
+// Package Searching
+app.get('/search', (req, res) => {
+    const query = req.query['query']
+    if (!query) {
+        // Send an empty list of results
+        res.send(kati.stringify({}))
+        return
+    }
+})
