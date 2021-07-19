7dbffe96a5e17ae977d6a9bf447ba55cf962f5b2
Joe Pea <joe@trusktr.io>
2021-07-18 18:21:49 -0700

    add a load animation and prevent the site from showing until the Wasm module has loaded
    Without this, the nav bar and footer show up a few moments after the initial content is loaded, because it takes a few moments for the Wasm module to get loaded and executed.

--- aspkg-bss/assembly/index.ts
+++ aspkg-bss/assembly/index.ts
@@ -1,5 +1,17 @@
 // Export AssemblyScript-side glue code or not everything will work (for example the customElements API).
 export * from '../node_modules/asdom/assembly/glue'
 
+import { document } from '../node_modules/asdom/assembly'
+import { setTimeout } from '../node_modules/ecmassembly/assembly/index'
 import './elements/NavBar'
 import './elements/TheFooter'
+
+let loader = document.querySelector('.heart-loader-container')!
+
+setTimeout(() => {
+    loader.setAttribute('class', loader.getAttribute('class') + ' fadeout')
+
+    setTimeout(() => {
+        loader.remove()
+    }, 1000)
+}, 1000)
